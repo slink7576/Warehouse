c7a5ced22eb6d7db056e3f39553d8d93d75ddbe1
Sergiy Popovych <slnkonline2@gmail.com>
2018-01-30 17:26:48 +0200

    upd

--- Warehouse/Global.asax.cs
+++ Warehouse/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Http;
 
 namespace Warehouse
 {
@@ -12,6 +13,7 @@ namespace Warehouse
     {
         protected void Application_Start()
         {
+           
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
